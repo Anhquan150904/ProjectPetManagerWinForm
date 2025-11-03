386e5293b6a837c694eba75ac11336e3f473f5a5
Wuan_26092004 <phamhuyen1115204@gmail.com>
2025-11-03 19:13:08 +0700

    check lại

--- PetManagerWinForm/Program.cs
+++ PetManagerWinForm/Program.cs
@@ -12,6 +12,7 @@ namespace PetManagerWinForm
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new FormDangNhap());
+
         }
     }
 }
