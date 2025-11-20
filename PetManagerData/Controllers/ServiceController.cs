12fd2d95d5bb774e8b6048481a9721c77390f7df
diggory1102 <thienkhoi2004@gmail.com>
2025-11-20 17:41:36 +0700

    Update code dich vu

---
+++ PetManagerData/Controllers/ServiceController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace PetManagerData.Controllers
+{
+    public class ServiceController
+    {
+        private readonly string _connStr;
+
+        public ServiceController(string connectionString)
+        {
+            _connStr = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public DataTable GetServices()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Services";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    da.Fill(dt);
+                }
+            }
+
+            return dt;
+        }
+
+        public bool DeleteService(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                conn.Open();
+                string query = "DELETE FROM Services WHERE ServiceId = @id";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public bool AddService(string name, string type, int amount, decimal price)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                conn.Open();
+                string query = @"INSERT INTO Services (ServiceName, Type, Amount, Price)
+                                 VALUES (@name, @type, @amount, @price)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@name", name ?? string.Empty);
+                    cmd.Parameters.AddWithValue("@type", type ?? string.Empty);
+                    cmd.Parameters.AddWithValue("@amount", amount);
+                    cmd.Parameters.AddWithValue("@price", price);
+
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public bool UpdateService(int id, string name, string type, int amount, decimal price)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                conn.Open();
+                string query = @"UPDATE Services
+                                 SET ServiceName = @name,
+                                     Type = @type,
+                                     Amount = @amount,
+                                     Price = @price
+                                 WHERE ServiceId = @id";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@name", name ?? string.Empty);
+                    cmd.Parameters.AddWithValue("@type", type ?? string.Empty);
+                    cmd.Parameters.AddWithValue("@amount", amount);
+                    cmd.Parameters.AddWithValue("@price", price);
+
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public DataTable SearchServices(string searchString)
+        {
+            DataTable dt = new DataTable();
+            string pattern = $"%{searchString}%";
+
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                conn.Open();
+                string query = @"SELECT * FROM Services
+                                 WHERE (ServiceName LIKE @pattern OR
+                                        Type LIKE @pattern OR
+                                        CONVERT(NVARCHAR(50), Price) LIKE @pattern)";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@pattern", pattern);
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        // Additional methods can be added later as needed
+    }
+}
