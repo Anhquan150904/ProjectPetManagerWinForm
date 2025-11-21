aa7e613ecd54ed366027d32d13f0bb769d59d6ca
hoangviet <hoangviet@HOANGVIET>
2025-11-21 22:25:53 +0700

    nhan vien

--- PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.Designer.cs
+++ PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.Designer.cs
@@ -28,12 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "QuanLyNhanVien";
+            dataGridView1 = new DataGridView();
+            textID = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textName = new TextBox();
+            label3 = new Label();
+            textAddress = new TextBox();
+            label4 = new Label();
+            textPhone = new TextBox();
+            label5 = new Label();
+            textSex = new TextBox();
+            label6 = new Label();
+            textEmail = new TextBox();
+            label7 = new Label();
+            textPosition = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(743, 342);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textID
+            // 
+            textID.Location = new Point(874, 15);
+            textID.Name = "textID";
+            textID.Size = new Size(237, 23);
+            textID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(789, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(21, 15);
+            label1.TabIndex = 2;
+            label1.Text = "ID:";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(789, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Name:";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textName
+            // 
+            textName.Location = new Point(874, 57);
+            textName.Name = "textName";
+            textName.Size = new Size(237, 23);
+            textName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(789, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Address";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textAddress
+            // 
+            textAddress.Location = new Point(874, 101);
+            textAddress.Name = "textAddress";
+            textAddress.Size = new Size(237, 23);
+            textAddress.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(789, 147);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 15);
+            label4.TabIndex = 8;
+            label4.Text = "PhoneNumber:";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textPhone
+            // 
+            textPhone.Location = new Point(874, 144);
+            textPhone.Name = "textPhone";
+            textPhone.Size = new Size(237, 23);
+            textPhone.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(789, 192);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Sex:";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textSex
+            // 
+            textSex.Location = new Point(874, 192);
+            textSex.Name = "textSex";
+            textSex.Size = new Size(237, 23);
+            textSex.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(789, 237);
+            label6.Name = "label6";
+            label6.Size = new Size(39, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Email:";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textEmail
+            // 
+            textEmail.Location = new Point(874, 234);
+            textEmail.Name = "textEmail";
+            textEmail.Size = new Size(237, 23);
+            textEmail.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(789, 286);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Position:";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textPosition
+            // 
+            textPosition.Location = new Point(874, 283);
+            textPosition.Name = "textPosition";
+            textPosition.Size = new Size(237, 23);
+            textPosition.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(789, 331);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnAdd_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(874, 331);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 16;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(955, 331);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 17;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1036, 331);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 18;
+            button4.Text = "Detail";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // QuanLyNhanVien
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1136, 369);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(textPosition);
+            Controls.Add(label6);
+            Controls.Add(textEmail);
+            Controls.Add(label5);
+            Controls.Add(textSex);
+            Controls.Add(label4);
+            Controls.Add(textPhone);
+            Controls.Add(label3);
+            Controls.Add(textAddress);
+            Controls.Add(label2);
+            Controls.Add(textName);
+            Controls.Add(label1);
+            Controls.Add(textID);
+            Controls.Add(dataGridView1);
+            Name = "QuanLyNhanVien";
+            Text = "Quan Ly Nhan Vien";
+            Load += QuanLyNhanVien_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textID;
+        private Label label1;
+        private Label label2;
+        private TextBox textName;
+        private Label label3;
+        private TextBox textAddress;
+        private Label label4;
+        private TextBox textPhone;
+        private Label label5;
+        private TextBox textSex;
+        private Label label6;
+        private TextBox textEmail;
+        private Label label7;
+        private TextBox textPosition;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.cs
+++ PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace PetManagerWinForm.NghiepVu.QLNhanVien
 {
     public partial class QuanLyNhanVien : Form
     {
+        private DataTable dtNhanVien = new DataTable();
         public QuanLyNhanVien()
         {
             InitializeComponent();
+        }
+        private void QuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            dtNhanVien.Columns.Add("ID", typeof(int));
+            dtNhanVien.Columns.Add("Name", typeof(string));
+            dtNhanVien.Columns.Add("Address", typeof(string));
+            dtNhanVien.Columns.Add("PhoneNumber", typeof(string));
+            dtNhanVien.Columns.Add("Sex", typeof(string));
+            dtNhanVien.Columns.Add("Email", typeof(string));
+            dtNhanVien.Columns.Add("Position", typeof(string));
+            dataGridView1.DataSource = dtNhanVien;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (!int.TryParse(textID.Text, out int id))
+            {
+                MessageBox.Show("ID không hợp lệ. Vui lòng nhập số nguyên.", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textID.Focus();
+                return;
+            }
+
+            // Tạo một hàng dữ liệu mới
+            DataRow newRow = dtNhanVien.NewRow();
+
+            // Gán giá trị từ TextBox vào các cột tương ứng
+            newRow["ID"] = id;
+            newRow["Name"] = textName.Text;
+            newRow["Address"] = textAddress.Text;
+            newRow["PhoneNumber"] = textPhone.Text;
+            newRow["Sex"] = textSex.Text;
+            newRow["Email"] = textEmail.Text;
+            newRow["Position"] = textPosition.Text;
+
+            // Thêm hàng vào DataTable (DataGridView sẽ tự động cập nhật)
+            dtNhanVien.Rows.Add(newRow);
+
+
+            textID.Text = string.Empty;
+            textName.Text = string.Empty;
+            textAddress.Text = string.Empty;
+            textPhone.Text = string.Empty;
+            textSex.Text = string.Empty;
+            textEmail.Text = string.Empty;
+            textPosition.Text = string.Empty;
+            textID.Focus();
         }
     }
 }
