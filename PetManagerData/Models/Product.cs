70cc198b1d29e5168f84fbe9f4d70da01c96f67f
Vũ Quân <vuquan150904@gmail.com>
2025-11-21 22:21:25 +0700

    lam do

---
+++ PetManagerData/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetManagerData.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Country { get; set; }
+        public string Condition { get; set; }
+    }
+}
--- PetManagerWinForm/NghiepVu/QLSanPham/QuanLySanPham.cs
+++ PetManagerWinForm/NghiepVu/QLSanPham/QuanLySanPham.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetManagerData.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,46 @@ namespace PetManagerWinForm.NghiepVu.QLSanPham
         public QuanLySanPham()
         {
             InitializeComponent();
+        }
+
+        private void ThongTinSanPham_Load()
+        {
+            // Code khởi tạo form và load dữ liệu khách hàng
+            var product = new List<Product>()
+            {
+                   
+            };
+            dvgProduct.DataSource = product;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ShowAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
