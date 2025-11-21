a826592c0519322aa4cf1dd0169bb5ae8a108255
Hoang Viet <quachhoangviet2004@gmail.com>
2025-11-21 23:10:49 +0700

    thêm hard code

--- PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.Designer.cs
+++ PetManagerWinForm/NghiepVu/QLNhanVien/QuanLyNhanVien.Designer.cs
@@ -47,6 +47,9 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            button5 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -57,12 +60,12 @@
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(743, 342);
+            dataGridView1.Size = new Size(743, 427);
             dataGridView1.TabIndex = 0;
             // 
             // textID
             // 
-            textID.Location = new Point(874, 15);
+            textID.Location = new Point(874, 92);
             textID.Name = "textID";
             textID.Size = new Size(237, 23);
             textID.TabIndex = 1;
@@ -70,7 +73,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(789, 18);
+            label1.Location = new Point(778, 95);
             label1.Name = "label1";
             label1.Size = new Size(21, 15);
             label1.TabIndex = 2;
@@ -80,7 +83,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(789, 60);
+            label2.Location = new Point(778, 139);
             label2.Name = "label2";
             label2.Size = new Size(42, 15);
             label2.TabIndex = 4;
@@ -89,7 +92,7 @@
             // 
             // textName
             // 
-            textName.Location = new Point(874, 57);
+            textName.Location = new Point(874, 136);
             textName.Name = "textName";
             textName.Size = new Size(237, 23);
             textName.TabIndex = 3;
@@ -97,7 +100,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(789, 104);
+            label3.Location = new Point(778, 183);
             label3.Name = "label3";
             label3.Size = new Size(49, 15);
             label3.TabIndex = 6;
@@ -106,7 +109,7 @@
             // 
             // textAddress
             // 
-            textAddress.Location = new Point(874, 101);
+            textAddress.Location = new Point(874, 180);
             textAddress.Name = "textAddress";
             textAddress.Size = new Size(237, 23);
             textAddress.TabIndex = 5;
@@ -114,7 +117,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(789, 147);
+            label4.Location = new Point(778, 227);
             label4.Name = "label4";
             label4.Size = new Size(88, 15);
             label4.TabIndex = 8;
@@ -123,7 +126,7 @@
             // 
             // textPhone
             // 
-            textPhone.Location = new Point(874, 144);
+            textPhone.Location = new Point(874, 224);
             textPhone.Name = "textPhone";
             textPhone.Size = new Size(237, 23);
             textPhone.TabIndex = 7;
@@ -131,7 +134,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(789, 192);
+            label5.Location = new Point(778, 271);
             label5.Name = "label5";
             label5.Size = new Size(27, 15);
             label5.TabIndex = 10;
@@ -140,7 +143,7 @@
             // 
             // textSex
             // 
-            textSex.Location = new Point(874, 192);
+            textSex.Location = new Point(874, 268);
             textSex.Name = "textSex";
             textSex.Size = new Size(237, 23);
             textSex.TabIndex = 9;
@@ -148,7 +151,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(789, 237);
+            label6.Location = new Point(778, 315);
             label6.Name = "label6";
             label6.Size = new Size(39, 15);
             label6.TabIndex = 12;
@@ -157,7 +160,7 @@
             // 
             // textEmail
             // 
-            textEmail.Location = new Point(874, 234);
+            textEmail.Location = new Point(874, 312);
             textEmail.Name = "textEmail";
             textEmail.Size = new Size(237, 23);
             textEmail.TabIndex = 11;
@@ -165,7 +168,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(789, 286);
+            label7.Location = new Point(778, 359);
             label7.Name = "label7";
             label7.Size = new Size(53, 15);
             label7.TabIndex = 14;
@@ -174,14 +177,14 @@
             // 
             // textPosition
             // 
-            textPosition.Location = new Point(874, 283);
+            textPosition.Location = new Point(874, 356);
             textPosition.Name = "textPosition";
             textPosition.Size = new Size(237, 23);
             textPosition.TabIndex = 13;
             // 
             // button1
             // 
-            button1.Location = new Point(789, 331);
+            button1.Location = new Point(778, 408);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 15;
@@ -191,36 +194,68 @@
             // 
             // button2
             // 
-            button2.Location = new Point(874, 331);
+            button2.Location = new Point(864, 408);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 16;
             button2.Text = "Update";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += btnUpt_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(955, 331);
+            button3.Location = new Point(950, 408);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 17;
             button3.Text = "Delete";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += btnDel_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(1036, 331);
+            button4.Location = new Point(1036, 408);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 18;
             button4.Text = "Detail";
             button4.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(778, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(333, 23);
+            textBox1.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(778, 32);
+            label8.Name = "label8";
+            label8.Size = new Size(183, 15);
+            label8.TabIndex = 20;
+            label8.Text = "Tìm kiếm nhân viên (id/tên/vị trí)\r\n";
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.Control;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Location = new Point(1071, 50);
+            button5.Name = "button5";
+            button5.Size = new Size(40, 23);
+            button5.TabIndex = 21;
+            button5.Text = "Tìm";
+            button5.UseVisualStyleBackColor = false;
+            // 
             // QuanLyNhanVien
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1136, 369);
+            ClientSize = new Size(1136, 454);
+            Controls.Add(button5);
+            Controls.Add(label8);
+            Controls.Add(textBox1);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
@@ -270,5 +305,8 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private TextBox textBox1;
+        private Label label8;
+        private Button button5;
     }
 }
