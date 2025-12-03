8a6f53405f305b7daa4b30bce6c4360cc83d10cd
Vũ Quân <vuquan150904@gmail.com>
2025-12-04 02:57:57 +0700

    Nâng cấp giao diện + base lại kiến trúc 3 lớp

---
+++ PetManagerData/DataAccess/ServiceRepository.cs
@@ -0,0 +1,27 @@
+﻿using PetManagerData.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace PetManagerData.DataAccess
+{
+    public class ServiceRepository
+    {
+        private string _connStr;
+        public ServiceRepository(string connStr) { _connStr = connStr; }
+
+        public DataTable GetAll()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                string query = "SELECT * FROM Service";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    da.Fill(dt);
+                }
+            }
+            return dt;
+        }
+    }
+}
