45b6954c416769e832914fbce3f8fc096e5f6a85
hoangviet <hoangviet@HOANGVIET>
2025-12-09 16:50:03 +0700

    thêm chức năng cho phần quản lý nhân viên

---
+++ PetManagerWinForm/NghiepVu/QLNhanVien/FormPhanCa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data; // <--- Quan trọng: Phải có dòng này mới hiểu DataTable là gì
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetManagerWinForm.NghiepVu.QLNhanVien
+{
+    public partial class FormPhanCa : Form
+    {
+        // Tạo một biến để lưu trữ danh sách nhân viên được truyền sang
+        private DataTable duLieuNhanVien;
+
+        // --- SỬA DÒNG NÀY ---
+        // Thêm "DataTable dt" vào trong ngoặc
+        public FormPhanCa(DataTable dt)
+        {
+            InitializeComponent();
+
+            // Lưu dữ liệu nhận được vào biến của Form này để dùng sau này
+            this.duLieuNhanVien = dt;
+        }
+
+        private void FormPhanCa_Load(object sender, EventArgs e)
+        {
+            // Ví dụ: Kiểm tra xem dữ liệu đã qua chưa
+            // MessageBox.Show("Đã nhận được danh sách " + duLieuNhanVien.Rows.Count + " nhân viên!");
+        }
+    }
+}
--- PetManagerWinForm/NghiepVu/QLNhanVien/FormTinhLuong.Designer.cs
+++ PetManagerWinForm/NghiepVu/QLNhanVien/FormTinhLuong.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PetManagerWinForm.NghiepVu.QLNhanVien
 {
-    partial class Detail
+    partial class FormTinhLuong
     {
         /// <summary>
         /// Required designer variable.
@@ -28,16 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
-            // 
-            // Detail
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Detail";
-            Text = "Chi tiết nhân viên";
-            ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "FormTinhLuong";
         }
 
         #endregion
